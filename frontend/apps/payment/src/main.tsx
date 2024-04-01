5d9ea38edb9fddcc8608035dae40f6d60d7032dd
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2024-04-01 14:06:17 +0200

    Initial commit

---
+++ frontend/apps/payment/src/main.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Card } from "@repo/ui/card";
+import "./index.css"; //Import our own css
+import "@repo/ui/styles.css"; //import the ui css
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1 className="text-red-500 text-9xl">Min payment service :)</h1>
+    <Card />
+  </React.StrictMode>
+);
