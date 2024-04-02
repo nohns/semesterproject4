ca74573cc40b0b30e5a0ef525b42dc7227c5bc8b
MadsSoeborg <madssoborg@gmail.com>
2024-04-02 13:07:45 +0200

    test: added button to test shad

--- frontend/apps/payment/src/main.tsx
+++ frontend/apps/payment/src/main.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Card } from "@repo/ui/card";
+import { Button } from "@repo/ui/button";
 import "./index.css"; //Import our own css
 import "@repo/ui/styles.css"; //import the ui css
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <h1 className="text-red-500 text-9xl">Min payment service :)</h1>
     <Card />
+    <Button variant="outline">Knaaaap</Button>
   </React.StrictMode>
 );
