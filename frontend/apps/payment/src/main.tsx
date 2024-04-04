126e38c08a7a392029a30d8b3b3d2eee5f085056
MadsSoeborg <madssoborg@gmail.com>
2024-04-04 15:04:20 +0200

    Payment UI stuff

--- frontend/apps/payment/src/main.tsx
+++ frontend/apps/payment/src/main.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Card } from "@repo/ui/card";
-import { Button } from "@repo/ui/button";
 import { ProductCard } from "@repo/ui/productCard";
 import { productsMock } from "@repo/ui/data/productsMock";
 import "./index.css"; // Import your own css
@@ -16,10 +14,7 @@ const root = ReactDOM.createRoot(rootElement as HTMLElement);
 root.render(
   <React.StrictMode>
     <div>
-      <h1 className="text-red-500 text-9xl">Min payment service :)</h1>
-      <Card />
-      <Button variant="outline">Knaaaap</Button>
-      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="mt-8 grid grid-cols-1 gap-4">
         {productsMock.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
