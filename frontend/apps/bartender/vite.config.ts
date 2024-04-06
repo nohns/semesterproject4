117170a525c4a9b376eb04bd767d18650804ab43
Asger Nohns <asger@nohns.dk>
2024-04-06 23:10:26 +0200

    feat(frontend): static ports

--- frontend/apps/bartender/vite.config.ts
+++ frontend/apps/bartender/vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+      port: 5175,
+      host: '0.0.0.0',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
--- frontend/apps/payment/vite.config.ts
+++ frontend/apps/payment/vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+      port: 5174,
+      host: '0.0.0.0',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
