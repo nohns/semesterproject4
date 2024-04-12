1e772a8c33a5d8fbb4464bb0d032eb87d5135907
MadsSoeborg <madssoborg@gmail.com>
2024-04-12 11:41:44 +0200

    feat: initial admin page for bartender frontend

---
+++ frontend/apps/bartender/src/page/Admin.tsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+import NewDrinkCard from "@/components/ui/NewDrinkCard";
+import { BigScreenWrapper } from "@repo/ui";
+
+
+function Admin() {
+    return (
+        <>
+            <BigScreenWrapper>
+                <NewDrinkCard />
+            </BigScreenWrapper>
+        </>
+
+    )
+}
+
+export default Admin;
