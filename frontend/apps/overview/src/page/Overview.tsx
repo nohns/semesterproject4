5ed00d84e2a38e0ed9ec99ba12357773ac41205a
MadsSoeborg <madssoborg@gmail.com>
2024-05-08 12:15:19 +0200

    Using minimal graphs for SlidingBeverageItemCardsLigmaNamingIsHard

--- frontend/apps/overview/src/page/Overview.tsx
+++ frontend/apps/overview/src/page/Overview.tsx
@@ -17,13 +17,19 @@ const mockItems = [
 
 function Overview() {
   const mockPrices = [
-    { date: new Date("2023-01-01T00:00:00"), price: 100 },
-    { date: new Date("2023-01-02T00:00:00"), price: 105 },
+    { date: new Date("2023-01-01T01:00:00"), price: 100 },
+    { date: new Date("2023-01-01T02:00:00"), price: 105 },
+    { date: new Date("2023-01-01T03:00:00"), price: 110 },
+    { date: new Date("2023-01-01T04:00:00"), price: 120 },
+    { date: new Date("2023-01-01T05:00:00"), price: 130 },
+    { date: new Date("2023-01-01T06:00:00"), price: 50 },
+    { date: new Date("2023-01-01T13:00:00"), price: 75 },
+    { date: new Date("2023-01-01T17:00:00"), price: 101 },
   ];
 
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex flex-none" style={{ height: "20%" }}>
+      <div className="flex flex-none justify-center" style={{ height: "20%" }}>
         <FooBar />
       </div>
       <div className="flex flex-grow" style={{ height: "60%" }}>
