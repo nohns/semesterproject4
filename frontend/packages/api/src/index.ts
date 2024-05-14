390dbaa6c9d0f36a859748e4c51866d6efd3346a
MadsSoeborg <madssoborg@gmail.com>
2024-05-14 21:30:07 +0200

    bartender works again

--- frontend/packages/api/src/index.ts
+++ frontend/packages/api/src/index.ts
@@ -5,4 +5,5 @@ export * from "./axios/axios";
 export * from "./endpoints/useLogin";
 export * from "./endpoints/useGetBeverages";
 export * from "./endpoints/usePutBeverages";
+export * from "./endpoints/usePostBeverage";
 export * from "./state/prices";
