50c8e28a2530598055b12587eefcfa53ecf7919a
jrysz <jonas.rysz@gmail.com>
2024-05-17 12:36:31 +0200

    changed halftime from string to int

---
+++ BeveragePaymentApi/BeveragePaymentApi/Migrations/20240517103349_HalfTimeFromStringToInt.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeveragePaymentApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class HalfTimeFromStringToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HalfTime",
+                table: "Beverages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HalfTime",
+                table: "Beverages",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
