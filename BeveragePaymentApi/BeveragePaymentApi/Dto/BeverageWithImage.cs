890b2c33dfa88e6982f4dcf54699b3d2e239c005
Jonas Hjortshøj Rysz <202205608@post.au.dk>
2024-05-20 14:36:19 +0200

    removed id from dto

--- BeveragePaymentApi/BeveragePaymentApi/Dto/BeverageWithImage.cs
+++ BeveragePaymentApi/BeveragePaymentApi/Dto/BeverageWithImage.cs
@@ -4,7 +4,6 @@ namespace Dto;
 
 public class BeverageWithImageDto
 {
-    public int BeverageId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public double BasePrice { get; set; }
