e47b933e59df0b30463da41b70ec8621961812d6
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2024-05-30 20:45:57 +0200

    cleanup

--- frontend/apps/ordering/src/routes.tsx
+++ frontend/apps/ordering/src/routes.tsx
@@ -3,7 +3,6 @@
 import { BrowserRouter, Route, Routes as RouterRoutes } from "react-router-dom";
 
 import { AnimatePresence } from "framer-motion";
-import Selected from "./page/Selected";
 import Selection from "./page/Selection";
 import Receipt from "./page/Receipt";
 import Order from "./page/Order";
@@ -14,11 +13,8 @@ export const Routes = () => {
       <AnimatePresence mode="wait">
         <RouterRoutes>
           <Route path="/" element={<Selection />} />
-          <Route path="/selected" element={<Selected />} />;
-          <Route path="/selected/:orderId" element={<Selected />} />;
           <Route path="/order/:orderId" element={<Order />} />;
           <Route path="/receipt/:orderId" element={<Receipt />} />;
-          <Route path="*" element={<Selection />} />
         </RouterRoutes>
       </AnimatePresence>
     </BrowserRouter>
