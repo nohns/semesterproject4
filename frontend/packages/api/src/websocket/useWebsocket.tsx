83de25655862c774ae34fd2edcaa16533a0dc7e9
Christian <cvsejersen@gmail.com>
2025-08-06 14:42:38 +0200

    stop pings from frontend

--- frontend/packages/api/src/websocket/useWebsocket.tsx
+++ frontend/packages/api/src/websocket/useWebsocket.tsx
@@ -2,12 +2,6 @@
 
 import { useEffect, useRef, useState, useCallback } from "react";
 
-export const DEFAULT_HEARTBEAT = {
-  message: "ping",
-  timeout: 60000, // Time to wait for a pong response
-  interval: 25000, // Interval at which to send ping messages
-};
-
 export interface Beverage {
   beverageId: string;
   name: string;
@@ -51,31 +45,6 @@ export const useWebsocket = (url: string) => {
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const heartbeatTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  const sendMessage = useCallback((message: string) => {
-    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
-      wsRef.current.send(message);
-    }
-  }, []);
-
-  // Function to start the heartbeat
-  const startHeartbeat = useCallback(() => {
-    const { interval, message } = DEFAULT_HEARTBEAT;
-
-    const heartbeatInterval = setInterval(() => {
-      sendMessage(message);
-      // Reset the heartbeat timeout every time a ping message is sent
-      if (heartbeatTimeoutRef.current) {
-        clearTimeout(heartbeatTimeoutRef.current);
-      }
-      heartbeatTimeoutRef.current = setTimeout(() => {
-        console.error("Heartbeat failed: closing socket");
-        wsRef.current?.close();
-      }, DEFAULT_HEARTBEAT.timeout);
-    }, interval);
-
-    return () => clearInterval(heartbeatInterval);
-  }, [sendMessage]);
-
   useEffect(() => {
     const socket = new WebSocket(url);
 
@@ -113,26 +82,19 @@ export const useWebsocket = (url: string) => {
     socket.onopen = () => {
       console.log("Connection established");
       setIsConnected(true);
-      startHeartbeat();
     };
 
     socket.onmessage = (event) => {
-      // Reset the heartbeat timeout if a pong message is received
-      if (event.data === "pong" && heartbeatTimeoutRef.current) {
-        console.log("Pong received");
-        clearTimeout(heartbeatTimeoutRef.current);
-      } else {
-        //console.log("Message received:", event.data);
-        try {
-          const data = JSON.parse(event.data);
-          if (!isPriceHistoriesMessage(data)) {
-            console.error("Invalid message:", data);
-            return;
-          }
-          onStructuredMsg(data);
-        } catch (error) {
-          console.error("Failed to parse event data:", error);
+      //console.log("Message received:", event.data);
+      try {
+        const data = JSON.parse(event.data);
+        if (!isPriceHistoriesMessage(data)) {
+          console.error("Invalid message:", data);
+          return;
         }
+        onStructuredMsg(data);
+      } catch (error) {
+        console.error("Failed to parse event data:", error);
       }
     };
 
@@ -158,7 +120,7 @@ export const useWebsocket = (url: string) => {
         clearTimeout(heartbeatTimeoutRef.current);
       }
     };
-  }, [url, startHeartbeat]);
+  }, [url]);
 
   return { histories, isConnected };
 };
